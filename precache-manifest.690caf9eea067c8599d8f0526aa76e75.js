970beaa7b849c45daf609d7f1f03d26e429bfddd
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-03-27 02:32:48 +0530

    Updates

---
+++ precache-manifest.690caf9eea067c8599d8f0526aa76e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15df232a1c510d654768",
+    "url": "/calculator/static/css/main.71d616de.chunk.css"
+  },
+  {
+    "revision": "15df232a1c510d654768",
+    "url": "/calculator/static/js/main.7b890117.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "782431a7a9fd8d41f546",
+    "url": "/calculator/static/js/2.8b2938bc.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b47d594ae3f95669fb0615947f93f118",
+    "url": "/calculator/index.html"
+  }
+];
